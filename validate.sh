ecf3fb2eb7c30886104bb8d67975899f6acc14ef
Grebennikov Roman <grv@dfdx.me>
2022-06-22 14:57:51 +0200

    use older jsonschema3

--- validate.sh
+++ validate.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-jsonschema -i schemas/ai.metarank/user/1-0-0 -o pretty jsonschema.json
-jsonschema -i schemas/ai.metarank/item/1-0-0 -o pretty jsonschema.json
-jsonschema -i schemas/ai.metarank/ranking/1-0-0 -o pretty jsonschema.json
-jsonschema -i schemas/ai.metarank/interaction/1-0-0 -o pretty jsonschema.json
+set -euxo pipefail
+
+JSONSCHEMA=$(mktemp /tmp/jsonschema.XXXXXX)
+
+wget -O $JSONSCHEMA "https://json-schema.org/draft/2020-12/schema"
+
+jsonschema -i schemas/ai.metarank/user/1-0-0 $JSONSCHEMA
+jsonschema -i schemas/ai.metarank/item/1-0-0 $JSONSCHEMA
+jsonschema -i schemas/ai.metarank/ranking/1-0-0 $JSONSCHEMA
+jsonschema -i schemas/ai.metarank/interaction/1-0-0 $JSONSCHEMA
+
+echo "validation successful"
