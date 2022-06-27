8363bc188a71fc4855c26633c9f465730821ca0e
Grebennikov Roman <grv@dfdx.me>
2022-06-27 14:34:19 +0200

    there should be /jsonschema prefix for all schemas in the path

--- validate.sh
+++ validate.sh
@@ -6,9 +6,9 @@ JSONSCHEMA=$(mktemp /tmp/jsonschema.XXXXXX)
 
 wget -O $JSONSCHEMA "https://json-schema.org/draft/2020-12/schema"
 
-jsonschema -i schemas/ai.metarank/user/1-0-0 $JSONSCHEMA
-jsonschema -i schemas/ai.metarank/item/1-0-0 $JSONSCHEMA
-jsonschema -i schemas/ai.metarank/ranking/1-0-0 $JSONSCHEMA
-jsonschema -i schemas/ai.metarank/interaction/1-0-0 $JSONSCHEMA
+jsonschema -i schemas/ai.metarank/user/jsonschema/1-0-0 $JSONSCHEMA
+jsonschema -i schemas/ai.metarank/item/jsonschema/1-0-0 $JSONSCHEMA
+jsonschema -i schemas/ai.metarank/ranking/jsonschema/1-0-0 $JSONSCHEMA
+jsonschema -i schemas/ai.metarank/interaction/jsonschema/1-0-0 $JSONSCHEMA
 
 echo "validation successful"
